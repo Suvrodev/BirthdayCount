65ece354c18e2e993c4d026738a583dd2edac4d7
suvrodeb <suvrodev.cse.77@gmail.com>
2023-10-07 15:56:30 +0600

    Added Feedback

--- src/Pages/Admin/Feedback/Feedback.jsx
+++ src/Pages/Admin/Feedback/Feedback.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useContext, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { AuthContext } from "../../../Provider/AuthProvider";
 
 const Feedback = () => {
-    return (
-        <div>
-            <h1>Feedback</h1>
-        </div>
-    );
+    const {successfullToast}=useContext(AuthContext)
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        // console.log("form: ",form.current.name);
+    
+        emailjs.sendForm('service_fup4rjf', 'template_aovt5cz', form.current, '_kzTXrFfQk2uFNUe9')
+          .then((result) => {
+              console.log(result);
+              if(result.status===200){
+                successfullToast("Sent your feedback")
+              }
+          }, (error) => {
+            console.log("Error:");
+              console.log(error.text);
+          });
+      };
+
+    
+  return (
+    <div>
+      <div className="border-white border-2 m-2 p-5 rounded-xl w-8/12 mx-auto">
+        <h1 className="bg-orange-500 text-center p-2 w-4/12 mx-auto font-bold rounded-md">Send Your Feedback</h1>
+        <form ref={form} onSubmit={sendEmail}>
+          <label className="font-bold">Name</label> <br />
+          <input className="w-full p-4 rounded-md my-4" type="text" name="user_name" placeholder="Write Your Name" /> <br />
+          <label className="font-bold">Email</label> <br />
+          <input className="w-full p-4 rounded-md my-4" type="email" name="user_email" placeholder="Write Your Mail" /> <br />
+          <label>Feedback</label> <br />
+          <textarea className="w-full h-[125px] p-4 rounded-md my-4 resize-none" name="message" placeholder="Write Message" /> <br />
+          <input className="btn btn-primary" type="submit" value="Send" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Feedback;
