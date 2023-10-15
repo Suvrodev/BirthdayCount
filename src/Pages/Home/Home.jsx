c172a58ab625fa00e883d40f234917467c60dc6d
suvrodeb <suvrodev.cse.77@gmail.com>
2023-10-16 00:13:09 +0600

    Added Homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import img1 from '../../assets/Gallery/1.png'
+import img2 from '../../assets/Gallery/2.jpg'
+import img3 from '../../assets/Gallery/3.png'
+import img4 from '../../assets/Gallery/4.png'
+import img5 from '../../assets/Gallery/5.png'
+import img6 from '../../assets/Gallery/6.png'
 const Home = () => {
     return (
-        <div className='  overflow-auto flex flex-col items-center justify-center'>
-            <div>
-                <h1 className='bg-green-600 p-5 m-5 w-[90%] md:w-4/12 text-white text-4xl text-center rounded-lg mx-auto'>WelCome To Birthday Count</h1>
-                <div className='flex justify-center items-center'>
-                    <img className='w-[90%] mx-auto' src={img1} alt="" />
-                </div>
-            </div>
+        <div className='  overflow-auto '>
+            <h1 className='bg-green-600 p-5 m-5 w-[90%] md:w-6/12 text-white text-4xl text-center rounded-lg mx-auto'>WelCome To Birthday Count</h1>
+                <div className='grid grid-cols-2 md:grid-cols-3 gap-5  p-5'>
+                    <img className='w-[250px] flex items-center justify-center' src={img1} alt="" />
+                    <img className='w-[250px] flex items-center justify-center' src={img2} alt="" />
+                    <img className='w-[250px] flex items-center justify-center' src={img3} alt="" />
+                    <img className='w-[250px] flex items-center justify-center' src={img4} alt="" />
+                    <img className='w-[250px] flex items-center justify-center' src={img5} alt="" />
+                    <img className='w-[250px] flex items-center justify-center' src={img6} alt="" />
+               </div>
         </div>
     );
 };
